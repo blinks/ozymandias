0ac0f2803781d5b5b5ab9f8688baab2f77b0ec87
Adam Blinkinsop <blinks@acm.org>
2015-04-06 15:31:25 -0700

    Fixed static path serving configuration.

--- xn--t-g9n/ozymandias.go
+++ xn--t-g9n/ozymandias.go
@@ -1,14 +1,12 @@
 package ozymandias
 
 import (
-  "html/template"
   "io/ioutil"
   "net/http"
 )
 
 var (
   staticIndex []byte
-  gameTmpl = template.Must(template.ParseFiles("static/game.html"))
 )
 
 func init() {
@@ -22,10 +20,3 @@ func init() {
 }
 
 func root(w http.ResponseWriter, r *http.Request) { w.Write(staticIndex) }
-
-func read(w http.ResponseWriter, r *http.Request) {
-  err := gameTmpl.Execute(w, MakeGame())
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-  }
-}
