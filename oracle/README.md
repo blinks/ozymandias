7ffc728e52dcdcfca46156bf9ba1fa01b0d05c89
Adam Blinkinsop <blinks@acm.org>
2017-03-17 10:18:09 -0700

    Refactored squib config.
    https://github.com/andymeneely/squib/issues/193 means we need to use
    Ruby 2.3.2 for now.

---
+++ oracle/README.md
@@ -0,0 +1,3 @@
+# Print & Play Notes
+
+TODO: Fill this out to give tips on how to play this with print and play.
--- oracle/deck.rb
+++ oracle/deck.rb
@@ -1,6 +1,8 @@
 require 'squib'
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
-  text str: 'Hello, World!'
-  save format: :png
+Squib::Deck.new(cards: 60, layout: 'economy.yml') do
+  background color: 'white'
+  rect layout: 'cut'
+  rect layout: 'safe'
+  save_png
 end
