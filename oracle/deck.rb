d8fc8e8e79eb4e96618ad55b5743bee51592ee75
Adam Blinkinsop <blinks@acm.org>
2017-03-17 10:57:30 -0700

    Very simple layout, no card text yet.
    Fixes #30

--- oracle/deck.rb
+++ oracle/deck.rb
@@ -1,8 +1,25 @@
 require 'squib'
+require 'game_icons'
 
-Squib::Deck.new(cards: 60, layout: 'economy.yml') do
+# Build a custom file format (or YAML?) that allows for card comments.
+data = Squib.csv file: 'data.csv'
+layouts = ['layout.yml']
+suits = {
+  'merchant' => GameIcons.get('lorc/ouroboros')
+    .recolor(fg: 'gold', bg_opacity: 0),
+  'soldier' => GameIcons.get('lorc/layered-armor')
+    .recolor(fg: 'red', bg_opacity: 0),
+  'sage' => GameIcons.get('lorc/erlenmeyer')
+    .recolor(fg: 'blue', bg_opacity: 0),
+}
+
+Squib::Deck.new cards: data['title'].size, layout: layouts do
   background color: 'white'
   rect layout: 'cut'
   rect layout: 'safe'
+  svg data: data['suit'].map { |k| suits[k].string }, layout: 'suit'
+  text str: data['rank'], layout: 'rank'
+  text str: data['title'], layout: 'title'
+  text str: data['text'], layout: 'text'
   save_png
 end
